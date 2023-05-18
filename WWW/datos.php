e424564f11cc46d9278df4d3f44dc293af0bb396
EmberBUhaj <a345541@uach.mx>
2023-05-17 20:00:49 -0600

    guardado de datos por si se va la luz

--- WWW/datos.php
+++ WWW/datos.php
@@ -9,7 +9,7 @@ $mensaje = $_POST['mensaje'];
 
 $datos = "Nombre: " . $nombre . "\n" . "Email: " . $email . "\n" . "Mensaje: " . $mensaje . "\n\n";
 
-$archivo = fopen("datos.txt", "a");
+$archivo = fopen("datosRecolectados.txt", "a");
 fwrite($archivo, $datos);
 fclose($archivo);
 
